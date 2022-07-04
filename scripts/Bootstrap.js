0f51927dff27198935701613f671897d197a3ef2
alon <alon.steimberg@gmail.com>
2022-07-04 20:12:32 +0300

    added bootstrap for controller

---
+++ scripts/Bootstrap.js
@@ -0,0 +1,10 @@
+import BoardController from "./BoardController/boardController.js";
+import HtmlView from "../views/htmlView.js"
+import NumericBoard from "./Board/numericBoard.js";
+
+export default function GetController(){ 
+    let view = new HtmlView()
+    let board = new NumericBoard(view.getLength())
+    let controller = new BoardController(board, view)
+    return controller
+}
